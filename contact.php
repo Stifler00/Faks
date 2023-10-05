f9d14296ce6fce4b3866c1fc5890e70d92418e10
Stifler00 <72310729+Stifler00@users.noreply.github.com>
2023-10-05 19:43:21 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,13 @@
+<article>
+    <h1>Kontakt</h1>
+    <form>
+        <input type="text" name="firstname" id="firstname" placeholder="Ime" required>
+        <input type="text" name="lastname" id="lastname" placeholder="Prezime" required>
+        <input type="email" name="email" id="email" placeholder="E-mail" required>
+        <textarea name="poruka"></textarea>
+        <input type="submit" value="posalji">
+
+    </form>
+
+
+</article>

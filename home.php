4a2fe353f26629c5a1005c081ea43ed1fad62001
Stifler00 <72310729+Stifler00@users.noreply.github.com>
2023-10-05 19:35:02 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,14 @@
+<article>
+        <p></p>
+        <h1>Žena (84) u 2 ujutro partijala u stanu u Bjelovaru, susjedi pozvali policiju</h1>
+        <figure>
+        <img src="slike/slika.jpg" alt="slika" title="slika" height="300">
+        </figure>
+        <p>BJELOVARSKA je policija sinoć morala intervenirati u jednom stanu nakon što je zaprimila dojavu o narušavanju javnog reda i mira. Naime, iz jednog se stana u centru grada čula glasna muzika od koje susjedi nisu mogli spavati te su pozvali policiju.</p>
+        <p>Po dolasku &nbsp; &nbsp; &nbsp; &nbsp; na mjesto događaja oko 2:30, policajci su u stanu zatekli 84-godišnjakinju koja je u tom trenutku bila sama i puštala glasnu muziku. Kako javlja policija, time je "ometala noćni mir drugih stanara u zgradi".</p>
+        <h2>Entiteti</h2>
+        <p>Manje - &lt;</p>
+        <p>Veće - &gt;</p>
+        <p>Apostrof - &apos;</p>
+        <p>Navodnik - &quot;</p>
+        </article>

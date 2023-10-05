56fe8acbf4b221d0edaeb9a3aba32d265514afa6
Stifler00 <72310729+Stifler00@users.noreply.github.com>
2023-10-05 19:21:04 +0200

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php  
+     print '<ul> 
+       <?php include ("menu.php")?>
+        <li><a href="index.php">Početna</a></li>
+        <li><a href="onama.php">O nama</a></li>
+        <li><a href="kontakt.php">Kontakt</a></li>
+        <li><a href="galerija.php">Galerija</a></li>
+       </ul>'
+   ?>

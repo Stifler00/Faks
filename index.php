f415e41610c8e8c5a51c0fedf8f228cf4840b71b
Stifler00 <72310729+Stifler00@users.noreply.github.com>
2023-10-05 19:20:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+print '<!doctype html>
+<html>
+    <head>
+        <meta>
+        <title>Zadatak 1</title>
+    </head>
+
+    <body>
+        <header>
+            <nav>' 
+                    <?php include ("menu.php")?>
+                print'
+            </nav>
+        </header>
+        <main>
+            <article>
+                <p></p>
+                <h1>Žena (84) u 2 ujutro partijala u stanu u Bjelovaru, susjedi pozvali policiju</h1>
+                <figure>
+                    <img src="slike/slika.jpg" alt="slika" title="slika" height="300">
+                </figure>
+                <p>BJELOVARSKA je policija sinoć morala intervenirati u jednom stanu nakon što je zaprimila dojavu o narušavanju javnog reda i mira. Naime, iz jednog se stana u centru grada čula glasna muzika od koje susjedi nisu mogli spavati te su pozvali policiju.</p>
+                <p>Po dolasku &nbsp; &nbsp; &nbsp; &nbsp; na mjesto događaja oko 2:30, policajci su u stanu zatekli 84-godišnjakinju koja je u tom trenutku bila sama i puštala glasnu muziku. Kako javlja policija, time je "ometala noćni mir drugih stanara u zgradi".</p>
+                <h2>Entiteti</h2>
+                <p>Manje - &lt;</p>
+                <p>Veće - &gt;</p>
+                <p>Apostrof - &apos;</p>
+                <p>Navodnik - &quot;</p>
+            </article>
+        </main>
+        <footer>
+            <p>Copyright &copy; Jan Tovernić <a href="https://github.com/Stifler00" target="_blank"> <img src="slike/slika2.png" alt="slika2" height="300"></a></p>
+            
+        </footer>
+    </body>
+</html>'
+?>
